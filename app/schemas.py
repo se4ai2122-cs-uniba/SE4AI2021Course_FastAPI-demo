3907b354e97b0e3aa75d32b2bb220d9429c28df5
Luigi Quaranta <quarantalgu@gmail.com>
2021-11-04 01:22:16 +0100

    Added example value for PredictPayload

--- app/schemas.py
+++ app/schemas.py
@@ -10,6 +10,16 @@ class PredictPayload(BaseModel):
     petal_length: float
     petal_width: float
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "sepal_length": 6.4,
+                "sepal_width": 2.8,
+                "petal_length": 5.6,
+                "petal_width": 2.1,
+            }
+        }
+
 
 class IrisType(Enum):
     setosa = 0
