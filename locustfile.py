53a48c3799f2a30a62046fecd77659c30f87b49d
Luigi Quaranta <quarantalgu@gmail.com>
2021-12-09 18:20:52 +0100

    Introduces randomness in the simulated locust requests

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,4 @@
-import time
+import random
 from locust import HttpUser, task, between
 
 
@@ -12,19 +12,19 @@ class TypicalIrisUser(HttpUser):
     @task(5)
     def logreg_prediction(self):
         flower_data = {
-            "sepal_length": 6.4,
-            "sepal_width": 2.8,
-            "petal_length": 5.6,
-            "petal_width": 2.1,
+            "sepal_length": random.uniform(4.3, 7.9),
+            "sepal_width": random.uniform(2.0, 4.4),
+            "petal_length": random.uniform(1.0, 6.9),
+            "petal_width": random.uniform(0.1, 2.5),
         }
         self.client.post("/models/LogisticRegression", json=flower_data)
 
     @task(5)
     def SVC_prediction(self):
         flower_data = {
-            "sepal_length": 6.4,
-            "sepal_width": 2.8,
-            "petal_length": 5.6,
-            "petal_width": 2.1,
+            "sepal_length": random.uniform(4.3, 7.9),
+            "sepal_width": random.uniform(2.0, 4.4),
+            "petal_length": random.uniform(1.0, 6.9),
+            "petal_width": random.uniform(0.1, 2.5),
         }
         self.client.post("/models/SVC", json=flower_data)
