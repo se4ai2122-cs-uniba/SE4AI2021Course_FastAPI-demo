de375649b14bcf962a64f55209f327dae7b28b6a
Luigi Quaranta <luigi.quaranta@uniba.it>
2022-12-06 11:07:36 +0100

    Add main to model training script

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,6 @@
 """Sample training script"""
 
+import json
 import pickle
 from pathlib import Path
 from typing import List
@@ -9,7 +10,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.svm import SVC
 
-
 MODELS_DIR = Path("models/")
 model_wrappers_list: List[dict] = []
 
@@ -18,97 +18,128 @@ model_wrappers_list: List[dict] = []
 # Data preparation #
 # ================ #
 
-# Load data
-Iris_data = load_iris()
 
-# Split data
-Xtrain, Xtest, Ytrain, Ytest = train_test_split(
-    Iris_data.data,  # pylint: disable=no-member
-    Iris_data.target,  # pylint: disable=no-member
-    test_size=0.3,
-    random_state=4,
-)
+def load_data():
+    """Load data and split it into train and test sets"""
+
+    # Load data
+    Iris_data = load_iris()
+
+    # Split data
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(
+        Iris_data.data,  # pylint: disable=no-member
+        Iris_data.target,  # pylint: disable=no-member
+        test_size=0.3,
+        random_state=4,
+    )
+
+    return Xtrain, Xtest, Ytrain, Ytest
+
 
 # ================== #
 # LogisticRegression #
 # ================== #
 
-print("Creating logistic regression model...")
 
-# Define the Logistic Regression model
-LR_parameters = {
-    "C": 0.1,
-    "max_iter": 20,
-    "fit_intercept": True,
-    "solver": "liblinear",
-    "random_state": 0,
-}
-LR_model = LogisticRegression(**LR_parameters)
+def train_log_reg_model(Xtrain, Ytrain):
+    """Train a Logistic Regression model"""
 
-# Train the Model
-LR_model.fit(Xtrain, Ytrain)
+    print("Creating logistic regression model...")
 
-# Evaluate model accuracy by cross-validation
-LR_accuracies = cross_val_score(estimator=LR_model, X=Xtrain, y=Ytrain, cv=10)
-LR_metrics = {"accuracy": LR_accuracies.mean()}
+    # Define the Logistic Regression model
+    LR_parameters = {
+        "C": 0.1,
+        "max_iter": 20,
+        "fit_intercept": True,
+        "solver": "liblinear",
+        "random_state": 0,
+    }
+    LR_model = LogisticRegression(**LR_parameters)
 
-# Wrap the trained model in a Model namedtuple and append it to the model list
-LR_model_dict = {
-    "type": "LogisticRegression",
-    "params": LR_parameters,
-    "model": LR_model,
-    "metrics": LR_metrics,
-}
-model_wrappers_list.append(LR_model_dict)
+    # Train the Model
+    LR_model.fit(Xtrain, Ytrain)
 
-print("Logistic regression model created.")
-print(LR_model_dict, end="\n\n\n")
+    # Evaluate model accuracy by cross-validation
+    LR_accuracies = cross_val_score(estimator=LR_model, X=Xtrain, y=Ytrain, cv=10)
+    LR_metrics = {"accuracy": LR_accuracies.mean()}
+
+    # Wrap the trained model in a Model namedtuple and append it to the model list
+    LR_model_dict = {
+        "type": "LogisticRegression",
+        "params": LR_parameters,
+        "metrics": LR_metrics,
+    }
+    print("\nLogistic regression model created.\n")
+    print(json.dumps(LR_model_dict, indent=4))
+
+    LR_model_dict["model"] = LR_model
+    model_wrappers_list.append(LR_model_dict)
 
 
 # =============================== #
 # C-Support Vector Classification #
 # =============================== #
 
-print("Creating SVC model...")
 
-# Define the SVC model
-SVC_parameters = {"kernel": "linear", "random_state": 0}
+def train_svc_model(Xtrain, Ytrain):
+    """Train a SVC model"""
 
-SVC_model = SVC(**SVC_parameters)
+    print("\n\nCreating SVC model...")
 
-# Train the Model
-SVC_model.fit(Xtrain, Ytrain)
+    # Define the SVC model
+    SVC_parameters = {"kernel": "linear", "random_state": 0}
 
-# Evaluate model accuracy by cross-validation
-SVC_accuracies = cross_val_score(estimator=SVC_model, X=Xtrain, y=Ytrain, cv=10)
-SVC_metrics = {"accuracy": SVC_accuracies.mean()}
+    SVC_model = SVC(**SVC_parameters)
 
-# Wrap the trained model in a Model namedtuple and append it to the model list
-SVC_model_dict = {
-    "type": "SVC",
-    "params": SVC_parameters,
-    "model": SVC_model,
-    "metrics": SVC_metrics,
-}
+    # Train the Model
+    SVC_model.fit(Xtrain, Ytrain)
 
-model_wrappers_list.append(SVC_model_dict)
+    # Evaluate model accuracy by cross-validation
+    SVC_accuracies = cross_val_score(estimator=SVC_model, X=Xtrain, y=Ytrain, cv=10)
+    SVC_metrics = {"accuracy": SVC_accuracies.mean()}
 
-print("SVC model created.")
-print(SVC_model_dict, end="\n\n\n")
+    # Wrap the trained model in a Model namedtuple and append it to the model list
+    SVC_model_dict = {
+        "type": "SVC",
+        "params": SVC_parameters,
+        "metrics": SVC_metrics,
+    }
+    print("\nSVC model created.\n")
+    print(json.dumps(SVC_model_dict, indent=4))
+
+    SVC_model_dict["model"] = SVC_model
+    model_wrappers_list.append(SVC_model_dict)
 
 
 # ============= #
 # Serialization #
 # ============= #
 
-print("Serializing model wrappers...")
 
-for wrapped_model in model_wrappers_list:
+def serialize_models():
+    """Serialize the trained models"""
 
-    pkl_filename = f"{wrapped_model['type']}_model.pkl"
-    pkl_path = MODELS_DIR / pkl_filename
+    print("\nSerializing model wrappers...")
 
-    with open(pkl_path, "wb") as file:
-        pickle.dump(wrapped_model, file)
+    for wrapped_model in model_wrappers_list:
 
-print("Serializing completed.")
+        pkl_filename = f"{wrapped_model['type']}_model.pkl"
+        pkl_path = MODELS_DIR / pkl_filename
+
+        with open(pkl_path, "wb") as file:
+            pickle.dump(wrapped_model, file)
+
+    print("Serializing completed.")
+
+
+# ============ #
+# Main program #
+# ============ #
+
+if __name__ == "__main__":
+    Xtrain, Xtest, Ytrain, Ytest = load_data()
+
+    train_log_reg_model(Xtrain, Ytrain)
+    train_svc_model(Xtrain, Ytrain)
+
+    serialize_models()
