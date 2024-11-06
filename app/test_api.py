4cce30969d73359c9d4f2ef697d365c2202917ff
Luigi Quaranta <luigi.quaranta@uniba.it>
2024-11-06 23:15:16 +0100

    Fix tests

--- app/test_api.py
+++ app/test_api.py
@@ -2,34 +2,37 @@ from fastapi.testclient import TestClient
 
 from .api import app
 
-client = TestClient(app)
-
 
 def test_root_endpoint():
     """Test the root endpoint."""
 
-    response = client.get("/")
+    with TestClient(app) as client:
 
-    assert response.status_code == 200
-    assert response.json()["method"] == "GET"
-    assert (
-        response.json()["data"]["message"]
-        == "Welcome to IRIS classifier! Please, read the `/docs`!"
-    )
+        response = client.get("/")
+
+        assert response.status_code == 200
+        assert response.json()["method"] == "GET"
+        assert (
+            response.json()["data"]["message"]
+            == "Welcome to IRIS classifier! Please, read the `/docs`!"
+        )
 
 
 def test_logreg_model_endpoint():
     """Test the logreg model endpoint."""
 
-    response = client.post(
-        "/models/SVC",
-        headers={"Content-Type": "application/json"},
-        json={
-            "sepal_length": 6.4,
-            "sepal_width": 2.8,
-            "petal_length": 5.6,
-            "petal_width": 2.1,
-        },
-    )
-    assert response.status_code == 200
-    assert response.json()["data"]["predicted_type"] == "virginica"
+    with TestClient(app) as client:
+
+        response = client.post(
+            "/models/LogisticRegression",
+            headers={"Content-Type": "application/json", "accept": "application/json"},
+            json={
+                "sepal_length": 6.4,
+                "sepal_width": 2.8,
+                "petal_length": 5.6,
+                "petal_width": 2.1,
+            },
+        )
+        assert response.status_code == 200
+        print(response.json())
+        assert response.json()["data"]["predicted_type"] == "virginica"
